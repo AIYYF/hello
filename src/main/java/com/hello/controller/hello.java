841f319272b9883a93e9ab3c7705edf3c4f1662f
zhouhaotian <2357417712@qq.com>
2019-06-06 08:12:59 +0800

    添加项目

---
+++ src/main/java/com/hello/controller/hello.java
@@ -0,0 +1,8 @@
+package com.hello.controller;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("上传到git上");
+        System.out.println("helloworld");
+    }
+}
